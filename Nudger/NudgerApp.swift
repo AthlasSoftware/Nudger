14680da5cf821b035b6742a9a6756d9c7fc2236b
CH <43988179+C-HGP@users.noreply.github.com>
2025-10-27 15:56:20 +0100

    Initial Commit

---
+++ Nudger/NudgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgerApp.swift
+//  Nudger
+//
+//  Created by Carl Henric Prestor on 2025-10-27.
+//
+
+import SwiftUI
+
+@main
+struct NudgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NudgerTests/NudgerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NudgerTests.swift
+//  NudgerTests
+//
+//  Created by Carl Henric Prestor on 2025-10-27.
+//
+
+import Testing
+@testable import Nudger
+
+struct NudgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
